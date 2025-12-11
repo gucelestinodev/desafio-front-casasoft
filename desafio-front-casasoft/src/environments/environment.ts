c44299b22972fcd7d5d8036bf7d92ea289d3cfcc
gucelestinodev <gulima.cursos@gmail.com>
2025-12-11 12:26:33 -0300

    fix: :green_heart: Ajuste para subir a build na Versel

--- desafio-front-casasoft/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  AUTH_BASE: 'https://casasoftchamado.casasoftsig.net.br/autenticacao',
-  CHAMADO_BASE: 'https://casasoftchamado.casasoftsig.net.br/chamado',
-} as const;
